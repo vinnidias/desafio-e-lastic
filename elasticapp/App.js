bd7dad6d72075b452fa30cb9284b8140ceeb9855
vinnidias <vinicius.3991@gmail.com>
2020-12-12 17:30:32 -0300

    separação das views

--- elasticapp/App.js
+++ elasticapp/App.js
@@ -3,16 +3,35 @@ import React from 'react';
 import {
   View,
   Text,
+  Image
 } from 'react-native';
+import styles from './src/styles'
 
 
 
 const App = ()=> {
   return(
-    <View>
-      <Text>
-        Foi
-      </Text>
+    <View style={styles.mainContainer}>
+      
+      <View style={styles.headerContainer}>
+        <Image 
+          source={require('./src/images/logo-color-small.png')}  
+          style={styles.image}
+        />
+      </View>
+
+      <View style={styles.infoContainer}>
+        <Text>
+          infos
+        </Text>
+      </View>
+
+      <View style={styles.exerciseContainer}>
+        <Text>
+          Exercícios
+        </Text>
+      </View>
+
     </View>
   )
 
