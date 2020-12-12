10d031209914d74eccce848f7dc75563daf04596
vinnidias <vinicius.3991@gmail.com>
2020-12-12 13:43:33 -0300

    react native cli setup

---
+++ elasticapp/App.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+
+const App = ()=> {
+  return(
+    <View>
+      <Text>
+        Foi
+      </Text>
+    </View>
+  )
+
+};
+
+export default App
