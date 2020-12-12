69d72f7809a9e36e1ede650e5a50934a92bac5b9
vinnidias <vinicius.3991@gmail.com>
2020-12-12 17:30:49 -0300

    carta de estilos

---
+++ elasticapp/src/styles.js
@@ -0,0 +1,25 @@
+const styles = {
+    mainContainer: {
+        flex: 1
+    },
+    headerContainer: {
+        height: 60, 
+        backgroundColor: '#ff6347',
+        flexDirection: 'row',
+        padding: 10
+    },
+    image:{
+        height: 40,
+        width: 200,
+    },
+
+    infoContainer:{
+
+    },
+    exerciseContainer: {
+
+    },
+    
+}
+
+export default styles
